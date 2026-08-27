53a23a57657ec154eb2dfc2e3d9da1d990e3cbbe
Nandhakumar <43298331+Nandhu29696@users.noreply.github.com>
2026-08-27 18:32:24 +0530

    Delete .next directory

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"PP4P8ZPqewmPOvk22bqzw2bTSlFpfJXgg1adUqNEsVE=\"\n}"
--- .next/standalone/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wjMOODL2aLG/1QXyHWnvK5ndgKpqPSpcON5AhbOuwFc=\"\n}"
